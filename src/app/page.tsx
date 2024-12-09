a36c9391d5bd2e93fa76ec8b9e9785d4e25e2773
kurutodenzeru <kurtdenzel51@gmail.com>
2024-12-09 15:09:47 +0800

    fix: adjust padding in BarcodeGenerator component for improved layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -70,7 +70,7 @@ export default function BarcodeGenerator() {
 	}, [barcodeData, barcodeConfig, textDecoration]);
 
 	return (
-		<div className="container mx-auto px-4 py-12 items-center justify-center flex">
+		<div className="container mx-auto px-4 py-8 items-center justify-center flex">
 			<div className="absolute inset-0 -z-10 h-full w-full bg-white dark:bg-gray-900 bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] dark:bg-[radial-gradient(#2a2a2a_1px,transparent_1px)] [background-size:16px_16px]" />
 			<div className="container mx-auto">
 				<div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:flex-row-reverse">
