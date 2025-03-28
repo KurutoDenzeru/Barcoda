08c5e22774518619cf151d3cd282d36da2e67bf2
kurutodenzeru <kurtdenzel51@gmail.com>
2025-03-28 23:49:05 +0800

    refactor(metadata): simplify sitemap and update layout metadata

--- src/app/sitemat.ts
+++ src/app/sitemat.ts
@@ -2,13 +2,11 @@ import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://barcoda.vercel.app'
-  
+
   return [
     {
       url: baseUrl,
       lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 1,
     }
   ]
 }
